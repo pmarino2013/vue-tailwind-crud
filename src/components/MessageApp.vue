3d2443c11926a8a4a43a9c4b0852999bed9d0940
Pablo Marino <mentor.rolling@gmail.com>
2023-12-31 17:53:50 -0300

    Creo componente básico para mensaje en caso de borrar un usuario

---
+++ src/components/MessageApp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-2 bg-slate-100 rounded-lg w-1/2">
+    <div class="flex justify-end px-3">
+      <button>X</button>
+    </div>
+    <h3 class="text-lg font-bold text-center mb-3">
+      Are you sure you want to delete this user?
+    </h3>
+  </div>
+</template>
+<script setup></script>
+<style scope></style>
